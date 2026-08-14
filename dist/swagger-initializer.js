ec8df8c681497ced6451ffc38959121aa56b448b
Katharina Brückmann <159028784+kbrueckmann@users.noreply.github.com>
2026-08-14 11:17:40 +0200

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,19 +3,20 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "../dts.yaml",
+    urls: [
+      {url: "../dts.yaml"}
+    ],
     dom_id: '#DTS',
     deepLinking: true,
     queryConfigEnabled: false,
     presets: [
       SwaggerUIBundle.presets.apis,
-      // SwaggerUIStandalonePreset
+      SwaggerUIStandalonePreset
     ],
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    //layout: "StandaloneLayout"
-    layout: "BaseLayout"
+    layout: "StandaloneLayout"
   });
 
   //</editor-fold>
